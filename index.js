aec12ad3d8eb7389847278c38f93fc2b7135305f
R Narayan <53508229+R-Narayan@users.noreply.github.com>
2020-07-31 15:40:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-var listOfCommands = ['help' , 'clear'];
+var listOfCommands = ['help', 'clear'];
+var his = document.getElementById("history");
+var textInput = document.querySelector("#input");
+
+textInput.focus()
+
 var commandInfo = {
     'help': "lists all the commands",
-    'clear':"clears the terminal<br><br>"//give 2 br at the end of last cmd
+    'clear': "clears the terminal<br><br>"//give 2 br at the end of last cmd
 }
 var historyCommands = '';
 var clText = 'Hack webpage>'//this should be changed
-var his = document.getElementById("history");
 console.log(his)
-var textInput = document.querySelector("#input");
 function comm(ele) {
-    if(event.key === 'Enter') {
+    if (event.key === 'Enter') {
         var command = textInput.value;
-        console.log(command);   
+        console.log(command);
         textInput.value = "";
         historyCommands += (clText + ' ' + command + '<br>');
-        if(listOfCommands.includes(command)){
+        if (listOfCommands.includes(command)) {
             commandFunction(command);
         }
-        else{
+        else {
             historyCommands += "<br>'" + command + "'" + " is not a recognized command<br> type 'help' to see the list of commands<br><br>";
         }
 
@@ -25,14 +28,17 @@ function comm(ele) {
         his.innerHTML = historyCommands;
     }
 }
-function commandFunction(c){
-    if(c === 'help'){
-        for(var i = 0 ; i<listOfCommands.length;i++){
-            historyCommands += "<br>" + listOfCommands[i] + " : " + commandInfo[listOfCommands[i]] ;
+function commandFunction(c) {
+    if (c === 'help') {
+        for (var i = 0; i < listOfCommands.length; i++) {
+            historyCommands += "<br>" + listOfCommands[i] + " : " + commandInfo[listOfCommands[i]];
         }
     }
-    if(c === 'clear'){
+    if (c === 'clear') {
         historyCommands = "";
         textInput.value = "";
     }
+}
+const foc = () => {
+    textInput.focus()
 }
